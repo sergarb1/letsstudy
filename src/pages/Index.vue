10a068f35abd509fdf5fab6577b0bc42a2a11b8a
sergarb1 <sergi.profesor@gmail.com>
2020-06-07 10:33:56 +0200

    Primer Commit: ¡Estudia!

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Lets Study logo"
+      src="~assets/logo_letsstudy.png"
+    >
+    <h1>¡ ESTUDIA !</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
