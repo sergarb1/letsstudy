718db41af12edfbca71e3dd6fb178c5d5e788e34
VicentBG <vbatallerg@gmail.com>
2020-06-12 16:09:37 +0200

    Añadidos comentarios y funcion data

--- src/clases/FuncionesAuxiliares.js
+++ src/clases/FuncionesAuxiliares.js
@@ -5,13 +5,13 @@ class FuncionesAuxiliares {
     // Función que toma una cantidad de segundos y devuelve una cadena
     // que representa el tiempo en hh:mm:ss
     static segundosToText(s) {
-
-        if (s < 0 || s > 86399) return null
+        // Controlamos los límites posibles de los datos y nos aseguramos que sea un número entero
+        if (s < 0 || s > 86399 || !Number.isInteger(s)) return null
 
         let segundos = '00'
         let minutos = '00'
         let horas = '00'
-      
+        // Con esta asignación nos aseguramos que siempre devuelva un cero primero si es un valor inferior a 10
         segundos = ('0' + s % 60).slice(-2)
       
         if (s >= 60) {
@@ -56,28 +56,31 @@ class FuncionesAuxiliares {
 // Esto si lo comentais para probar, luego des-comentarlo
 export default FuncionesAuxiliares
 
-// Para test: comentar la línea del export y descomentar la siguientes líneas
-/*
+/**Tests segundosToText
 let tiempoEnTexto1 = FuncionesAuxiliares.segundosToText(1)
 let tiempoEnTexto2 = FuncionesAuxiliares.segundosToText(3600)
 let tiempoEnTexto3 = FuncionesAuxiliares.segundosToText(86399)
 let tiempoEnTexto4 = FuncionesAuxiliares.segundosToText(88000)
 let tiempoEnTexto5 = FuncionesAuxiliares.segundosToText(-153)
+let tiempoEnTexto6 = FuncionesAuxiliares.segundosToText("no soy un número")
 
 console.log(tiempoEnTexto1)
 console.log(tiempoEnTexto2)
 console.log(tiempoEnTexto3)
 console.log(tiempoEnTexto4)
 console.log(tiempoEnTexto5)
+console.log(tiempoEnTexto6)
 */
+
 /**Tests funcion minutosEntreFechas
 console.log(FuncionesAuxiliares.minutosEntreFechas("2020-6-15","2021-6-15"));
 console.log(FuncionesAuxiliares.minutosEntreFechas("November 12 2020 17:51","November 12 2020 17:52:55 "));
 console.log(FuncionesAuxiliares.minutosEntreFechas("November 12 2020 17:51","November 12 2020 18:52"));
 console.log ("introduciendo ms: "+ FuncionesAuxiliares.minutosEntreFechas(1000,5000));
- */
+*/
+
 /**Tests funcion segundosEntreFechas
 console.log(FuncionesAuxiliares.segundosEntreFechas("2000-01-02 00:00:00","January 1 2000 00:00:00"));
 console.log(FuncionesAuxiliares.minutosEntreFechas("January 2 2000 00:00:00","January 1 2000 00:00:00"));
 console.log ("introduciendo ms: "+ FuncionesAuxiliares.segundosEntreFechas(1600,5000));
- //*/
+*/
--- src/pages/Resumen.vue
+++ src/pages/Resumen.vue
@@ -1,21 +1,25 @@
+<!-- Otro componente Vue, que mostrará la página de las
+estadísticas de tiempo de estudio del usuario -->
 <template>
   <div class="q-pa-md column items-center q-gutter-md">
+    <!-- Aquí hacemos uso del componente de Quasar para crear
+    tarjetas -->
     <q-card class="my-card">
       <q-card-section class="bg-grey-3">
         <h4>Estudiado hoy:</h4>
-        <h3>2h. 59min.</h3>
+        <h3>{{ hoy }}</h3>
       </q-card-section>
     </q-card>
     <q-card class="my-card">
       <q-card-section class="bg-grey-3">
         <h4>Estudio semana:</h4>
-        <h3>8h. 22 min.</h3>
+        <h3>{{ semana }}</h3>
       </q-card-section>
     </q-card>
     <q-card class="my-card">
       <q-card-section  class="bg-grey-3">
         <h4>Total estudio:</h4>
-        <h3>55h. 25 min.</h3>
+        <h3>{{ total }}</h3>
       </q-card-section>
     </q-card>
   </div>
@@ -23,10 +27,23 @@
 
 <script>
 export default {
-  name: 'Resumen'
+  name: 'Resumen',
+  /* Creamos la función 'data' para hacer reactivas las
+  variables que contienen los tiempos de estudio. Esto en un futuro
+  se podría indicar como 'props' del componente. */
+  data: function() {
+    return {
+      hoy: "2h. 59min.",
+      semana: "8h. 22 min.",
+      total: "55h. 25 min."
+    };
+  }
 }
 </script>
 
+<!-- Aquí hacemos uso de la etiqueta 'style' usando lenguaje 'SASS'
+ para dar algún formato más al html de arriba. Al indicar 'scoped'
+ nos aseguramos que sólo afectará a este componente y ninguno más. -->
 <style lang="sass" scoped>
   .my-card
     width: 100%
