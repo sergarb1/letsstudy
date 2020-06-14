030cd76068f7e3c13324cb4816846868c23d430d
Cristina Chiarri <cristinachiarri@gmail.com>
2020-06-14 18:23:54 +0200

    Cambios apariencie AcerdaDe

--- src/pages/AcercaDe.vue
+++ src/pages/AcercaDe.vue
@@ -1,10 +1,32 @@
 <template>
+
   <q-page class="flex flex-center">
-    <div class="column justify-center">
-      <img alt="Lets Study logo" src="~assets/logo_letsstudy.png" />
-      <h3 class="text-center">Proyecto realizado en el Hackton 19/20 del CEEDCV</h3>
-    </div>
+    
+
+    <q-intersection transition="fade">
+      <q-card
+        class="my-card q-mb-sm .shadow-3 bg-teal-6 text-white text-center">
+        <q-card-section>
+        <div class="column justify-center">
+        <img alt="Lets Study logo" src="~assets/logo_letsstudy.png" />
+        </div>
+        </q-card-section>
+      </q-card>
+
+      <q-card
+      class="my-card q-mb-sm .shadow-3 bg-teal-6 text-white text-center">
+        <q-card-section>
+        <div class="text-h6">Proyecto realizado en el Hackaton 19/20 del CEEDCV</div>
+        
+        </q-card-section>
+
+      </q-card>
+    
+      
+    </q-intersection>
+
   </q-page>
+  
 </template>
 
 <script>
