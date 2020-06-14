4fea8ecc2acfd5a279ff77a3b3ea8f4ca8793197
VicentBG <vbatallerg@gmail.com>
2020-06-14 09:44:21 +0200

    Incluimos lista de frases motivadoras a Cronometro

--- src/pages/Cronometro.vue
+++ src/pages/Cronometro.vue
@@ -82,7 +82,28 @@ export default {
       valorInterval: null, // variable utilizada para parar el "setInterval"
       sesion: null, // variable usada para registrar las distintas sesiones
       // Creamos un usuario para pruebas, el usuario real se le pasará al componente como 'props'
-      usuario: new Usuario("Usuario de prueba")
+      usuario: new Usuario("Usuario de prueba"),
+      frasesMotivadoras: [
+        "Siempre parece imposible hasta que se hace (Nelson Mandela)",
+        "La motivación es lo que te pone en marcha, el hábito es lo que hace que sigas (Jim Ryun)",
+        "Si no te gusta cómo son las cosas, cámbialas (Jim Rohn)",
+        "No hay un sustituto para el trabajo duro (Thomas Edison)",
+        "Sin estudiar enferma el alma (Séneca)",
+        "Si hiciésemos todas las cosas de las que somos capaces, nos asombraríamos (Thomas Edison)",
+        "Cambia tus pensamientos y cambiarás tu mundo (Norman Vincent Peale)",
+        "Tus talentos y habilidades irán mejorando con el tiempo, pero para eso has de empezar (Martin Luther King)",
+        "Nuestra paciencia conseguirá más cosas que nuestra fuerza (Edmund Burke)",
+        'Los libros son peligrosos. Los mejores deberían ser etiquetados con un "Esto podría cambiar tu vida" (Helen Exley)',
+        "Si no vas hasta el final ¿por qué empezar? (Joe Namath)",
+        "Aprender sin pensar es inútil. Pensar sin aprender, peligroso (Confucio)",
+        "Lo maravilloso de aprender algo es que nadie puede arrebatárnoslo (B. B. King)",
+        "El hombre bien preparado para la lucha ha conseguido medio triunfo (Miguel de Cervantes)",
+        "Es posible recuperarse de una derrota, pero cuesta más perdonarse a uno mismo por no haberlo intentado (George Edward Woodberry)",
+        "Eres más que capaz de encontrar el éxito, pero solo ocurrirá si te pones a ello (Vince Lombardi)",
+        "La perseverancia puede transformar el fracaso en un logro extraordinario (Matt Biondi)",
+        "Sin autodisciplina, el éxito es imposible (Lou Holz)",
+        "La energía y la persistencia conquistan todas las cosas (Benjamin Franklin)"
+      ]
     };
   },
   // Definimos metodos del componente
@@ -156,7 +177,7 @@ export default {
     showNotifInicio() {
       this.$q.notify({
         message: "** Iniciada nueva sesión de estudio **",
-        caption: "¡Sumando tiempo conseguirás tus objetivos!",
+        caption: this.getFraseAleatoria(), // Con cada inicio de sesión nos pasará aleatoriamente, una frase de la lista
         color: "accent"
       });
     },
@@ -166,6 +187,12 @@ export default {
         caption: "Ahora puedes consultarla en el histórico...",
         color: "info"
       });
+    },
+    // Funcion para que nos devuelva una frase aleatoria de la lista
+    getFraseAleatoria() {
+      return this.frasesMotivadoras[
+        Math.floor(Math.random() * this.frasesMotivadoras.length)
+      ];
     }
   }
 };
