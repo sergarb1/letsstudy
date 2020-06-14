cbcd6dc0850b0a6b81d5a1eb8de6a7761be9d333
lionel-ict <lionel.ta@gmail.com>
2020-06-14 12:43:14 +0200

    Tests de FuncionesAuxiliares.segundosToText() hechos

--- src/clases/FuncionesAuxiliares.js
+++ src/clases/FuncionesAuxiliares.js
@@ -2,8 +2,9 @@
 
 class FuncionesAuxiliares {
 
-    // Función que toma una cantidad de segundos y devuelve una cadena
-    // que representa el tiempo en hh:mm:ss
+    // Convierte una cantidad de segundos a formato texto 'hh:mm:ss'
+    // s: segundos (nº entero) entre 0 y 86399
+    // return: cadena de texto entre '00:00:00' y '23:59:59', null si 's' no es válido
     static segundosToText(s) {
         // Controlamos los límites posibles de los datos y nos aseguramos que sea un número entero
         if (s < 0 || s > 86399 || !Number.isInteger(s)) return null
@@ -11,79 +12,42 @@ class FuncionesAuxiliares {
         let segundos = '00'
         let minutos = '00'
         let horas = '00'
+
         // Con esta asignación nos aseguramos que siempre devuelva un cero primero si es un valor inferior a 10
         segundos = ('0' + s % 60).slice(-2)
       
         if (s >= 60) {
-      
           s = parseInt(s / 60)
-      
           minutos = ('0' + s % 60).slice(-2)
       
           if (s >= 60) {
-      
             s = parseInt(s / 60)
-      
             horas = ('0' + s % 60).slice(-2)
-      
           }
-      
         }
       
-        return horas + " : " + minutos + " : " + segundos
-      
+        return horas + ":" + minutos + ":" + segundos
       }
 
-      //calcula los minutos que han pasado entre dos fechas, redondeando.
+      // Devuelve los minutos que han pasado entre dos fechas, truncado
       static minutosEntreFechas(fechaA,fechaB){
         let dateA = new Date(fechaA).getTime();
         let dateB = new Date(fechaB).getTime();
         let difS = Math.abs(dateA-dateB);
         let toMin = difS/60000;
-        return Math.round(toMin);
+        return Math.trunc(toMin);
       }
-      //Calcula los segundos que han pasado entre dos fechas,redondeando
+
+      // Devuelve los segundos que han pasado entre dos fechas, truncado
       static segundosEntreFechas(fechaA,fechaB){
         let dateA = new Date(fechaA).getTime();
         let dateB = new Date(fechaB).getTime();
         let difS = Math.abs(dateA-dateB);
         let toSecs = difS/1000;
-        return Math.round(toSecs);
+        return Math.trunc(toSecs);
     }
 
 }
 
-// Para testear con Mocha hace falta descomentar esta línea y comentar la de abajo
-//module.exports = FuncionesAuxiliares;
-
 // Esto si lo comentais para probar, luego des-comentarlo
 export default FuncionesAuxiliares
-
-/**Tests segundosToText
-let tiempoEnTexto1 = FuncionesAuxiliares.segundosToText(1)
-let tiempoEnTexto2 = FuncionesAuxiliares.segundosToText(3600)
-let tiempoEnTexto3 = FuncionesAuxiliares.segundosToText(86399)
-let tiempoEnTexto4 = FuncionesAuxiliares.segundosToText(88000)
-let tiempoEnTexto5 = FuncionesAuxiliares.segundosToText(-153)
-let tiempoEnTexto6 = FuncionesAuxiliares.segundosToText("no soy un número")
-
-console.log(tiempoEnTexto1)
-console.log(tiempoEnTexto2)
-console.log(tiempoEnTexto3)
-console.log(tiempoEnTexto4)
-console.log(tiempoEnTexto5)
-console.log(tiempoEnTexto6)
-*/
-
-/**Tests funcion minutosEntreFechas
-console.log(FuncionesAuxiliares.minutosEntreFechas("2020-6-15","2021-6-15"));
-console.log(FuncionesAuxiliares.minutosEntreFechas("November 12 2020 17:51","November 12 2020 17:52:55 "));
-console.log(FuncionesAuxiliares.minutosEntreFechas("November 12 2020 17:51","November 12 2020 18:52"));
-console.log ("introduciendo ms: "+ FuncionesAuxiliares.minutosEntreFechas(1000,5000));
-*/
-
-/**Tests funcion segundosEntreFechas
-console.log(FuncionesAuxiliares.segundosEntreFechas("2000-01-02 00:00:00","January 1 2000 00:00:00"));
-console.log(FuncionesAuxiliares.minutosEntreFechas("January 2 2000 00:00:00","January 1 2000 00:00:00"));
-console.log ("introduciendo ms: "+ FuncionesAuxiliares.segundosEntreFechas(1600,5000));
-*/
--- test/test_FuncionesAuxiliares.js
+++ test/test_FuncionesAuxiliares.js
@@ -1,40 +1,62 @@
 var assert = require('assert');
 import FuncionesAuxiliares from '../src/clases/FuncionesAuxiliares';
-//const fa = require('../src/clases/FuncionesAuxiliares');
 
 // Tests de la clase FuncionesAuxiliares
 describe('FuncionesAuxiliares', function () {
-    
-    //import FuncionesAuxiliares from 'src/clases/FuncionesAuxiliares'
 
     // Tests de la función segundosToText
     describe('segundosToText()', function () {
-        it('Entre 0 y 86400 segundos', function () {
+        it('Entre 0 y <86400 segundos', function () {
             assert.equal(FuncionesAuxiliares.segundosToText(0), "00:00:00");
             assert.equal(FuncionesAuxiliares.segundosToText(1), "00:00:01");
             assert.equal(FuncionesAuxiliares.segundosToText(59), "00:00:59");
             assert.equal(FuncionesAuxiliares.segundosToText(60), "00:01:00");
             assert.equal(FuncionesAuxiliares.segundosToText(61), "00:01:01");
-            assert.equal(FuncionesAuxiliares.segundosToText(120), "00:01:00");
-            assert.equal(FuncionesAuxiliares.segundosToText(121), "00:01:01");
+            assert.equal(FuncionesAuxiliares.segundosToText(119), "00:01:59");
+            assert.equal(FuncionesAuxiliares.segundosToText(120), "00:02:00");
+            assert.equal(FuncionesAuxiliares.segundosToText(121), "00:02:01");
             assert.equal(FuncionesAuxiliares.segundosToText(3540), "00:59:00");
             assert.equal(FuncionesAuxiliares.segundosToText(3541), "00:59:01");
+            assert.equal(FuncionesAuxiliares.segundosToText(3599), "00:59:59");
+            assert.equal(FuncionesAuxiliares.segundosToText(3600), "01:00:00");
+            assert.equal(FuncionesAuxiliares.segundosToText(3601), "01:00:01");
+            assert.equal(FuncionesAuxiliares.segundosToText(3660), "01:01:00");
+            assert.equal(FuncionesAuxiliares.segundosToText(3661), "01:01:01");
+            assert.equal(FuncionesAuxiliares.segundosToText(7199), "01:59:59");
+            assert.equal(FuncionesAuxiliares.segundosToText(7200), "02:00:00");
             assert.equal(FuncionesAuxiliares.segundosToText(86399), "23:59:59");
-            assert.equal(FuncionesAuxiliares.segundosToText(86400), "24:00:00");
+            //assert.equal(FuncionesAuxiliares.segundosToText(86400), "24:00:00");
         });
-        it('Más de 86400 segundos (24h)', function () {
-            assert.equal(FuncionesAuxiliares.segundosToText(86400), "24:00:00");
-            assert.equal(FuncionesAuxiliares.segundosToText(86401), "24:00:00");
-            assert.equal(FuncionesAuxiliares.segundosToText(90000), "24:00:00");
-            assert.equal(FuncionesAuxiliares.segundosToText(999999999), "24:00:00");
+        it('86400 segundos (24h) o más', function () {
+            assert.equal(FuncionesAuxiliares.segundosToText(86400), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(86401), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(90000), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(999999999), null);
         });
         it('Segundos negativos', function () {
-            assert.equal(FuncionesAuxiliares.segundosToText(-1), "00:00:00");
-            assert.equal(FuncionesAuxiliares.segundosToText(-10), "00:00:00");
-            assert.equal(FuncionesAuxiliares.segundosToText(-1000), "00:00:00");
-            assert.equal(FuncionesAuxiliares.segundosToText(-999999999), "00:00:00");
+            assert.equal(FuncionesAuxiliares.segundosToText(-1), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(-10), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(-1000), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(-999999999), null);
+        });
+        it('Parámetros que no son números enteros', function () {
+            assert.equal(FuncionesAuxiliares.segundosToText(null), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(7.0), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(7.5), null);
+            assert.equal(FuncionesAuxiliares.segundosToText('abc'), null);
+            assert.equal(FuncionesAuxiliares.segundosToText('10'), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(true), null);
+            assert.equal(FuncionesAuxiliares.segundosToText(false), null);
         });
     });
+
+    /*
+    //Tests funcion minutosEntreFechas
+    console.log(FuncionesAuxiliares.minutosEntreFechas("2020-6-15","2021-6-15"));
+    console.log(FuncionesAuxiliares.minutosEntreFechas("November 12 2020 17:51","November 12 2020 17:52:55 "));
+    console.log(FuncionesAuxiliares.minutosEntreFechas("November 12 2020 17:51","November 12 2020 18:52"));
+    console.log ("introduciendo ms: "+ FuncionesAuxiliares.minutosEntreFechas(1000,5000));
+    */
 
     /*
     // Tests de la función minutosEntreFechas
@@ -43,7 +65,16 @@ describe('FuncionesAuxiliares', function () {
             assert.equal(0, -1);
         });
     });
+    */
 
+    /*
+    // Tests funcion segundosEntreFechas
+    console.log(FuncionesAuxiliares.segundosEntreFechas("2000-01-02 00:00:00","January 1 2000 00:00:00"));
+    console.log(FuncionesAuxiliares.minutosEntreFechas("January 2 2000 00:00:00","January 1 2000 00:00:00"));
+    console.log ("introduciendo ms: "+ FuncionesAuxiliares.segundosEntreFechas(1600,5000));
+    */
+
+    /*
     // Tests de la función segundosEntreFechas
     describe('segundosEntreFechas(fechaA, fechaB)', function () {
         it('...', function () {
