41d15add8e06a2d1597a89d8a8f6699c1d7b6f01
ximocat <ximocat@gmail.com>
2020-06-19 17:55:57 +0200

    Añadidos enlaces a Github en AcercaDe.vue

--- src/pages/AcercaDe.vue
+++ src/pages/AcercaDe.vue
@@ -1,29 +1,27 @@
 <template>
-
   <q-page class="flex flex-center content-start">
-    <div class="column justify-center q-gutter-sm"  style="height: 250px">
-        <img alt="Lets Study logo" src="~assets/LETSSTUDY_2.png" />
-        </div>
+    <div class="column justify-center q-gutter-sm" style="height: 250px">
+      <img alt="Lets Study logo" src="~assets/LETSSTUDY_2.png" />
+    </div>
 
     <q-intersection transition="scale">
-      
-
       <q-card
-      class="q-mb-lg q-px-lg text-white bg-light-blue no-box-shadow text-left no-border-radius">
+        class="q-mb-lg q-px-lg text-white bg-light-blue no-box-shadow text-left no-border-radius"
+      >
         <q-card-section>
-        <h5>Proyecto realizado en el Hackaton 19/20 del CEEDCV</h5> 
-        <div class="text-subtitle1">Por unos cuantos alumnos y alumnas entregados al lado luminoso de 
-          la programación y unos profesores armados de paciencia infinita.</div>
-        
+          <h5>Proyecto realizado en el Hackaton 19/20 del CEEDCV</h5>
+          <div class="text-subtitle1">
+            Por unos cuantos alumnos y alumnas entregados al lado luminoso de
+            la programación y unos profesores armados de paciencia infinita.
+          </div>
+          <br>
+          <a href="https://github.com/sergarb1/letsstudy">Proyecto en Github</a>
+          <br>
+          <a href="https://github.com/sergarb1/letsstudy-downloads">Descarga LetsStudy</a>
         </q-card-section>
-
       </q-card>
-    
-      
     </q-intersection>
-
   </q-page>
-  
 </template>
 
 <script>
@@ -32,6 +30,6 @@ export default {
 };
 </script>
 <style lang="sass" scoped>
-  .fondo
-    background:#00cfee
+.fondo
+  background: #00cfee
 </style>
