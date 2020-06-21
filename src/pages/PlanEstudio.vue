6471a6e1d8d5ea350affa307b3f205e5598a19aa
ximocat <ximocat@gmail.com>
2020-06-21 19:33:24 +0200

    añadidos algunos textos

--- src/pages/PlanEstudio.vue
+++ src/pages/PlanEstudio.vue
@@ -1,28 +1,24 @@
+<!-- -->
+
 <template>
-  <q-page class="flex flex-center content-start">
-    <div class="column justify-center q-gutter-sm" style="height: 250px">
-      <img alt="Lets Study logo" src="~assets/LETSSTUDY_2.png" />
-    </div>
-
-    <q-intersection transition="scale">
-      <q-card
-        class="q-mb-lg q-px-lg text-white bg-light-blue no-box-shadow text-left no-border-radius"
-      >
-        <q-card-section>
-          <h5>prueba prueba</h5>
-          <div class="text-subtitle1">
-            rpeuba
+  <q-page class="flex column">
+    <div>
+      <div class="text-center Oswald">
+        <h4>
+          <u>PLAN DE ESTUDIO</u>
+        </h4>
+        <div>
+          <h4>Objetivos</h4>
+          <div>
+            <h5>Estudio diario: 2h</h5>
           </div>
-        </q-card-section>
-
-        
-      </q-card>
-    </q-intersection>
+          <h5>Estudio semanal: 12h</h5>
+          <h5>Estudio mensual: 2h</h5>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 </script>
-
-<style>
-</style>
